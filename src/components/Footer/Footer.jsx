af8495ca62bb419d757993d60fa9577ac1cd07bb
Vaibhav Agarwal <65801959+vaibhava17@users.noreply.github.com>
2022-08-08 20:10:26 +0530

    added sponsors button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,6 +18,7 @@ function AppFooter() {
             <a href="https://twitter.com/_vaibhava__" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-twitter" />
             </a>
+            <iframe src="https://github.com/sponsors/vaibhava17/button" title="Sponsor vaibhava17" height="35" width="116" style="border: 0;"></iframe>
           </span>
         </Row>
       </Container>
