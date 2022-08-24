887fe77c8fc479679ade930398030a4b7d5a410b
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2022-08-24 21:29:12 +0530

    dotenv config

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./responsive.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import "antd/dist/antd.css";
+require('dotenv').config();
 
 ReactDOM.render(
   <React.StrictMode>
