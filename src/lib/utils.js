dd2fad9a883776b7a4221cff7139daf34f1e896b
Vaibhav Agarwal <iamvaibhav.agarwal@gmail.com>
2025-06-17 18:32:07 +0530

    fix

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -34,19 +34,6 @@ export function debounce(func, wait) {
   };
 }
 
-export function throttle(func, limit) {
-  let inThrottle;
-  return function () {
-    const args = arguments;
-    const context = this;
-    if (!inThrottle) {
-      func.apply(context, args);
-      inThrottle = true;
-      setTimeout(() => (inThrottle = false), limit);
-    }
-  };
-}
-
 export function sanitizeInput(input) {
   if (typeof input !== "string") return input;
 
